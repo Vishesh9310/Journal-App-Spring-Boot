a61f210e695daad48325e52ea4c1c7d57e19eb4c
Vishesh9310 <129068125+Vishesh9310@users.noreply.github.com>
2025-07-18 18:47:48 +0530

    @Transactional Annotation

--- journalApp/src/main/java/com/vishesh/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/vishesh/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,12 +23,17 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry save = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(save);
-        userService.saveEntry(user);
+        try{
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry save = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(save);
+            userService.saveEntry(user);
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry){
